18271194a24734b94d4be72c2219d5628503eab9
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-12-10 13:13:35 -0500

    try to fix req.user

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,14 @@ async function bootstrap(): Promise<void> {
     credentials: true,
   });
 
+  if (process.env.NODE_ENV === 'production')
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+    });
+
   await app.listen(environment.PORT);
 }
 bootstrap();
