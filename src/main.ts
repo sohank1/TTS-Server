c0cddb4af2294f4174de93abe6796f30dd5ffb3b
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-12-10 13:16:50 -0500

    plz work

--- src/main.ts
+++ src/main.ts
@@ -19,17 +19,11 @@ async function bootstrap(): Promise<void> {
   );
   app.use(passport.initialize());
   app.use(passport.session());
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
 
-  if (process.env.NODE_ENV === 'production')
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-      next();
+  if (process.env.NODE_ENV !== 'production')
+    app.enableCors({
+      origin: true,
+      credentials: true,
     });
 
   await app.listen(environment.PORT);
