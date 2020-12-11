8d9eb9be541b9bc578dcf23c748a5ae9c017be04
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-12-11 10:08:14 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from "express-session";
 import * as passport from "passport";
-import * as cookieParser from 'cookie-parser';
+import * as cookieParser from "cookie-parser";
 import { environment } from "./environment/environment";
 
 async function bootstrap(): Promise<void> {
