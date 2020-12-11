a048b22530b01fd758fc3011edd544f8c4c7a377
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-12-11 10:34:05 -0500

    console.log user

--- src/auth/auth.serializer.ts
+++ src/auth/auth.serializer.ts
@@ -14,12 +14,12 @@ export class AuthSerializer extends PassportSerializer {
     }
 
     public serializeUser(user: User, done: (err: Error, user?: User) => void): void {
-        console.log('serializeUser...');
+        console.log('serializeUser...', user);
         done(null, user._id);
     }
 
     public async deserializeUser(id: string, done: (err: Error, user?: User) => void): Promise<void> {
-        console.log('deserializeUser...');
+        console.log('deserializeUser...', id);
         const user = await this.Users.findById(id);
         if (user) done(null, user);
     };
