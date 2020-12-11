2c672d5cbbce2a800242b820f1332a1e0baaa907
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2020-12-11 11:26:06 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["https://tts-app.netlify.app", "http://localhost:4200"],
+    origin: ["https://tts-app.netlify.app", "http://localhost:4200", "http://localhost:3000"],
     credentials: true,
   });
 
