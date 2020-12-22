6034ea5a0c6de4cf21161e84b2fa1ba5c93f6082
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2020-12-22 18:30:44 -0500

    Update client.middleware.ts

--- src/client.middleware.ts
+++ src/client.middleware.ts
@@ -22,6 +22,8 @@ const resolvePath = (file: string) => resolve(`./client/dist/TTS-Client/${file}`
 export class ClientMiddleware implements NestMiddleware {
     public use(req: Request, res: Response, next: NextFunction) {
         const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        
+        console.log(`URL: ${fullUrl} isHttp: ${process.env.NODE_ENV === 'production' && !fullUrl.startsWith('https')}`);
 
         if (process.env.NODE_ENV === 'production' && !fullUrl.startsWith('https')) {
             return res.redirect(fullUrl.replace('http', 'https'));
