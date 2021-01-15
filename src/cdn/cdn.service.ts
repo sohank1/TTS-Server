57163c38431836d876ce249f24266418c7716f79
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-01-15 11:07:57 -0500

    Update cdn.service.ts

--- src/cdn/cdn.service.ts
+++ src/cdn/cdn.service.ts
@@ -95,9 +95,10 @@ export class CdnService {
      * @param name - The old name of the file
      */
     private genFileName(name: string): string {
-        console.log('genFileName() { name }', name, "replace",  name.replace(/s+/g, '-'));
+        console.log('genFileName() { name }', name);
+        console.log("replace",  name?.replace(/s+/g, '-'));
         // Replace each space with a dash.
-        name = name.replace(/s+/g, '-');
+        name = name?.replace(/s+/g, '-');
 
         // Generate random 10 char string.
         const randomStr = `-${Math.random().toString(36).substr(2, 10)}`;
@@ -106,8 +107,8 @@ export class CdnService {
         // The Ëß are 2 special chars. A file name should never include Ëß or the random string will be inserted somewhere else.
         // It is "Ëß." because we want the element to start with "." instead of ending with it. "v1Ëß.4Ëß.jpg" => ["v1", ".4", "-a72qtaus68", ".jpg"] => "v1.4-a72qtaus68.jpg"
         // If it was ".Ëß" then it would be v1.,4.,jpg => ["v1.", "4.", "-ypzgia4rrl", "jpg"] => "v1.4.-ypzgia4rrljpg" 
-        console.log(`Name: ${name} Replace: ${name.replace(/\./gi, "Ëß.")} Split: ${name.replace(/\./gi, "Ëß.").split("Ëß")} `);
-        const arr = name.replace(/\./gi, "Ëß.").split("Ëß");
+        console.log(`Name: ${name} Replace: ${name?.replace(/\./gi, "Ëß.")} Split: ${name?.replace(/\./gi, "Ëß.").split("Ëß")} `);
+        const arr = name?.replace(/\./gi, "Ëß.").split("Ëß");
 
         // Insert the random string in the second to last element ["Penguin's-Advanced-ZoneScrim-Wars-V4", ".1", ".png"] => ["Penguin's-Advanced-ZoneScrim-Wars-V4", ".1", "-i318oex8xz", ".png"]
         arr.splice(arr.length - 1, 0, randomStr);
