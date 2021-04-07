b57d5a3ccaa3e3ab46f12cda7d277b9fa3133c3f
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-04-07 12:40:04 -0400

    Update app.component.ts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,6 +10,6 @@ export class AppComponent {
   public title = 'TTS-Client';
 
   constructor() {
-    setTimeout(() => AOS.init({ duration: 750 }), 1);
+    setTimeout(() => AOS.init({ duration: 750 }), 200);
   }
 }
